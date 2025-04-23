0136291b77fe0edbae4136bffc1e3a3db69c481c
Luke Sullivan <sully097@gmail.com>
2025-04-23 13:13:35 +0100

    Add simple mobile menu animation

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 import "./styles.css";
 
+// Set up event listeners for mobile menu
+const initApp = () => {
+    const hamburgerBtn = document.getElementById('hamburger-button');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    // Show/hide mobile menu
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.toggle('flex');
+    }
+
+    hamburgerBtn.addEventListener('click', toggleMenu);
+    mobileMenu.addEventListener('click', toggleMenu);
+}
+
 // Update footer year
 const year = document.getElementById('year');
 year.textContent = new Date().getFullYear();
+
+document.addEventListener('DOMContentLoaded', initApp);
