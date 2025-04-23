bb97cab95d6471c18755931be267fcee052b4c76
Luke Sullivan <sully097@gmail.com>
2025-04-23 12:37:57 +0100

    Add custom media queries

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'widescreen': { 'raw': '(min-aspect-ratio: 3/2)' },
+        'tallscreen': { 'raw': '(min-aspect-ratio: 1/2)' },
+      }
+    },
   },
   plugins: [],
 };
