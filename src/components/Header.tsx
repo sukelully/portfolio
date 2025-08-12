917f91f885db5a183903fe84f15518c026fdf985
Luke Sullivan <luke@sukelully.dev>
2025-08-12 11:04:43 +0100

    refactor: add HeroSection and Header components

---
+++ src/components/Header.tsx
@@ -0,0 +1,67 @@
+function Header() {
+  return (
+    <header className="sticky top-0 z-10 bg-teal-900 text-white">
+      <section className="mx-auto flex max-w-4xl items-center justify-between p-4">
+        <h1 className="text-3xl font-medium">sukelully.dev</h1>
+        <div>
+          <button
+            id="hamburger-button"
+            aria-label="Open mobile menu"
+            aria-expanded="false"
+            className="relative h-8 w-8 cursor-pointer text-3xl md:hidden"
+          >
+            <div className="absolute top-4 h-1 w-8 rounded bg-white transition-all duration-500 before:absolute before:h-1 before:w-8 before:-translate-x-4 before:-translate-y-3 before:rounded before:bg-white before:transition-all before:duration-500 before:content-[''] after:absolute after:h-1 after:w-8 after:-translate-x-4 after:translate-y-3 after:rounded after:bg-white after:transition-all after:duration-500 after:content-['']"></div>
+          </button>
+          <nav className="hidden space-x-8 text-xl md:block" aria-label="main">
+            <a href="#hello" className="hover:opacity-90">
+              Hello!
+            </a>
+            <a href="#projects" className="hover:opacity-90">
+              Projects
+            </a>
+            <a href="#about-me" className="hover:opacity-90">
+              About
+            </a>
+            <a href="#contact" className="hover:opacity-90">
+              Contact
+            </a>
+          </nav>
+        </div>
+      </section>
+
+      <section
+        id="mobile-menu"
+        className="animate-open-menu absolute top-68 hidden w-full origin-top flex-col justify-center bg-slate-100 text-5xl dark:bg-neutral-950"
+      >
+        <nav className="flex min-h-screen flex-col items-center py-8" aria-label="mobile">
+          <a
+            href="#hello"
+            className="w-full py-6 text-center text-black hover:opacity-90 dark:text-white"
+          >
+            Hello!
+          </a>
+          <a
+            href="#projects"
+            className="w-full py-6 text-center text-black hover:opacity-90 dark:text-white"
+          >
+            Projects
+          </a>
+          <a
+            href="#about-me"
+            className="w-full py-6 text-center text-black hover:opacity-90 dark:text-white"
+          >
+            About
+          </a>
+          <a
+            href="#contact"
+            className="w-full py-6 text-center text-black hover:opacity-90 dark:text-white"
+          >
+            Contact
+          </a>
+        </nav>
+      </section>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/HeroSection.tsx
@@ -0,0 +1,29 @@
+function HeroSection() {
+  return (
+    <section
+      id="hello"
+      className="widescreen:section-min-height tallscreen:section-min-height flex scroll-mt-40 flex-col-reverse items-center justify-center gap-8 p-6 sm:flex-row"
+    >
+      <article className="sm:w-1/2">
+        <h2 className="max-w-md text-center text-4xl font-bold text-slate-900 sm:text-left sm:text-5xl dark:text-white">
+          Full-Stack Developer with an
+          <span className="text-indigo-700 dark:text-indigo-300">Engineering Mindset</span>
+        </h2>
+        <p className="mt-4 max-w-md text-center text-2xl text-slate-700 sm:text-left dark:text-slate-400">
+          Hi! I'm a full-stack developer with a background in electronic engineering and audio
+          technology.
+        </p>
+      </article>
+      <div id="image-container" className="flex flex-row items-end gap-4">
+        <img
+          className="w-1/2"
+          src="./img/hello-pic.webp"
+          alt="A selfie of myself in an aircraft wearing a headset!"
+        />
+        <div id="typing-animation-div" className="h-3 w-1/3 bg-teal-900"></div>
+      </div>
+    </section>
+  );
+}
+
+export default HeroSection;
